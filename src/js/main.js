806a5a62616e882ff7c9aba9dc8e8a4f1e9eb727
赵宝 <zhaobao@lubanec.com>
2017-03-28 15:47:21 +0800

    页面完成

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+});
+
